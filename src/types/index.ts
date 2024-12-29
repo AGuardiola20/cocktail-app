7e1f42dff0b719f666e413947f70be63a4bf31a7
AGuardiola20 <andres.pguardiola@gmail.com>
2024-12-29 02:02:41 -0600

    add: DrinkCard

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import {
   CategoriesAPIResponseSchema,
+  DrinkAPIResponse,
   DrinksAPIResponse,
   SearchFilterShema,
 } from "../utils/recipes-schema";
@@ -10,3 +11,5 @@ export type Categories = z.infer<typeof CategoriesAPIResponseSchema>;
 export type SearchFilter = z.infer<typeof SearchFilterShema>;
 
 export type Drinks = z.infer<typeof DrinksAPIResponse>;
+
+export type Drink = z.infer<typeof DrinkAPIResponse>;
