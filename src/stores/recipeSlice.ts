3546d6387b7495dbe3145d0ceaff91c468841628
AGuardiola20 <andres.pguardiola@gmail.com>
2024-12-30 12:29:44 -0600

    add: Modal

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -10,9 +10,11 @@ export type RecipesSliceType = {
   categories: Categories;
   drinks: Drinks;
   selectedRecipe: Recipe;
+  modal: boolean;
   fetchCategories: () => Promise<void>;
   searchRecipes: (searchFilter: SearchFilter) => Promise<void>;
   selectRecipe: (id: Drink["idDrink"]) => Promise<void>;
+  closeModal: () => void;
 };
 
 export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
@@ -26,6 +28,7 @@ export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
     ingredients: [],
   },
   selectedRecipe: {} as Recipe,
+  modal: false,
   fetchCategories: async () => {
     const categories = await getCategories();
     set({
@@ -42,6 +45,13 @@ export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
     const selectedRecipe = await getRecipeById(id);
     set({
       selectedRecipe,
+      modal: true,
+    });
+  },
+  closeModal: () => {
+    set({
+      modal: false,
+      selectedRecipe: {} as Recipe,
     });
   },
 });
