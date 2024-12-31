1f2a69c82e30cc6c4c241cdf2c755c61317033fe
AGuardiola20 <andres.pguardiola@gmail.com>
2024-12-31 17:41:57 -0600

    add: react performance

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,8 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomeScreen from "./screens/HomeScreen";
 import Layout from "./layout/Layout";
-import FavoriteScreen from "./screens/FavoriteScreen";
+import HomeScreen from "./screens/HomeScreen";
+const FavoriteScreen = lazy(() => import("./screens/FavoriteScreen"));
 
 const AppRouter = () => {
   return (
@@ -9,7 +10,14 @@ const AppRouter = () => {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<HomeScreen />} index />
-          <Route path="/favoritos" element={<FavoriteScreen />} />
+          <Route
+            path="/favoritos"
+            element={
+              <Suspense fallback="Cargando...">
+                <FavoriteScreen />
+              </Suspense>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
