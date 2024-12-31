7d9c93e9fc6426f2869584c69d5cd166feb075be
AGuardiola20 <andres.pguardiola@gmail.com>
2024-12-31 14:53:35 -0600

    feat: close notification

--- src/stores/notificationSlice.ts
+++ src/stores/notificationSlice.ts
@@ -9,10 +9,12 @@ type Notification = {
 export type NotificationSliceType = {
   notification: Notification;
   showNotification: (payload: Pick<Notification, "text" | "error">) => void;
+  hideNotification: () => void;
 };
 
 export const createNotificationSlice: StateCreator<NotificationSliceType> = (
-  set
+  set,
+  get
 ) => ({
   notification: {
     text: "",
@@ -25,6 +27,18 @@ export const createNotificationSlice: StateCreator<NotificationSliceType> = (
         text: payload.text,
         error: payload.error,
         show: true,
+      },
+    });
+    setTimeout(() => {
+      get().hideNotification();
+    }, 5000);
+  },
+  hideNotification: () => {
+    set({
+      notification: {
+        text: "",
+        error: false,
+        show: false,
       },
     });
   },
